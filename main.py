34c68959606be300532a1af9effc72cf5aeb2db1
Steve McGregor <achieve@Steves-MacBook-Pro.local>
2023-07-05 22:17:33 -0400

    modified:   main.py modified:   save-path.txt

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from appJar import gui
+import tkinter.filedialog
+
+f = open("save-path.txt", "r")
+save_path = f.read()
 
 def setDates():
     startDate = app.getDatePicker("dp1")
     endDate = app.getDatePicker("dp2")
     print(startDate, endDate)
 
+def press(setFilePath):
+    save_path = tkinter.filedialog.askdirectory()
+    with open('save-path.txt', 'w') as file:
+        file.write(save_path)
+    app.setLabel("file_path", "Saving to: " + "\'" + save_path + "\'")
+    
 app = gui("vivtoolFetch")
 
 app.setBg("orange", override=True)
@@ -20,6 +30,8 @@ app.setDatePickerRange("dp1", 2010, 2050)
 app.setDatePickerRange("dp2", 2010, 2050)
 app.setDatePicker("dp1")
 app.setDatePicker("dp2")
+app.addButton("Set Save Location", press)
+app.addLabel("file_path", "Saving to: \'" + save_path + "\'")
 app.addButton("Retrieve Data", setDates)
 
 app.go()
