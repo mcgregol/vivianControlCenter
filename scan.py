af8c75cfecf7055b8ac96af782e6118089720941
Steve McGregor <achieve@Steves-MacBook-Pro.local>
2023-07-31 18:10:33 -0400

    modified:   main.py

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
 import subprocess
 
-scan_output = subprocess.run(['vivtool scan -v'], stdout=subprocess.PIPE)
+scan_output = subprocess.run(['vivtool', 'scan', '-v'], stdout=subprocess.PIPE)
 print(scan_output.stdout.decode('utf-8'))
