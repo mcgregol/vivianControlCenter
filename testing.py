6f3b14fb0a67ae6d1065261cef9c4058214e8d31
Liam McGregor <47013839+mcgregol@users.noreply.github.com>
2023-07-30 22:44:11 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,6 @@ from sensor import Sensor
 
 dylan = Sensor("DL43", "Dylan Larkin", 543543543543543)
 
-
 print("Player Name: " + dylan.get_name())
 print("Device ID: " + dylan.get_id())
 print("UUID: " + dylan.get_uuid())
