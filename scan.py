8f0c22127c0f85b3dca21c73b14b849677347a6b
Liam McGregor <liam@pop-os.localdomain>
2023-07-31 18:21:55 -0400

    modified:   scan.py

--- scan.py
+++ scan.py
@@ -3,6 +3,6 @@ import time
 
 scan_output = subprocess.run(['vivtool', 'scan', '-v'], stdout=subprocess.PIPE)
 
-for i in range(30):
+for i in range(15):
+    time.sleep(2)
     print(scan_output.stdout.decode('utf-8'))
-    time.sleep(1)
