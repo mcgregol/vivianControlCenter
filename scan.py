52bd05352e328c763d00af021b63d6b681cbc48a
Steve McGregor <achieve@Steves-MacBook-Pro.local>
2023-07-31 18:33:28 -0400

    modified:   main.py

--- scan.py
+++ scan.py
@@ -1,8 +1,14 @@
 import subprocess
-import time
 
-scan_output = subprocess.run(['vivtool', 'scan', '-v'], stdout=subprocess.PIPE)
+scan = subprocess.Popen('vivtool scan -v', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
-for i in range(15):
-    time.sleep(2)
-    print(scan_output.stdout.decode('utf-8'))
+while True:
+    scan_output = scan.stdout.readline().decode()
+    
+    if scan_output == '' and process.poll() is not None:
+        break
+        
+    if scan_output:
+        print(scan_output.strip())
+        
+rc = process.poll()
