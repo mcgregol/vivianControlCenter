62d46972fca04304432fdaf5ab590c62ac08b6f6
Steve McGregor <achieve@Steves-MacBook-Pro.local>
2023-08-02 15:14:09 -0400

    rewritten scan doesn't need live output

--- new_scan.py
+++ new_scan.py
@@ -1,18 +1,15 @@
 import subprocess
 import time
 
-command = ["vivtool", "scan", "-v"]  # Replace with your command and arguments
-
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen('vivtool scan', stdout=subprocess.PIPE, shell=True)
 
 try:
-    # Try to get the output for x seconds
-    stdout, stderr = process.communicate(timeout=15)
+    # Try to get the output for 15 seconds
+    stdout = process.communicate(timeout=5)
 except subprocess.TimeoutExpired:
-    # If the process does not end after x seconds, kill it
+    # If the process does not end after 15 seconds, kill it
     process.kill()
-    stdout, stderr = process.communicate()
+    stdout = process.communicate()
 
-# Now stdout and stderr will have the output and errors till x seconds after which the process was killed
+# Now stdout will have the output and errors till 15 seconds after which the process was killed
 print(stdout)
-print(stderr)
