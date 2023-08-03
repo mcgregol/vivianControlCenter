a7ba60787b0dde48a8c53020c6688ee1e1be89a0
Liam McGregor <liam@pop-os.localdomain>
2023-08-02 23:10:33 -0400

    modified:   scan.py

--- scan.py
+++ scan.py
@@ -3,7 +3,7 @@ import subprocess, time
 
 scan = subprocess.Popen('vivtool scan', shell=True, stdout=subprocess.PIPE)
 
-# change to 15 for irl use
+# change to 10-15 for irl use
 time.sleep(5)
 
 if scan.poll() is None:
@@ -11,7 +11,7 @@ if scan.poll() is None:
     time.sleep(0.5)
     if scan.poll() is None:
         scan.kill()
-        
+
 scan_output, _ = scan.communicate()
 
 pretty_string = scan_output.decode('utf-8')
@@ -23,5 +23,5 @@ dictionary = {}
 for line in lines[2:]:
     key, value = line.split(' ')
     dictionary[key] = value
-    
+
 print(dictionary)
