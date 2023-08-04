20f4adeb9b873de1c9c489f848fb54945f3dfb5d
Liam McGregor <47013839+mcgregol@users.noreply.github.com>
2023-08-03 22:55:14 -0400

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-from sensor import Sensor
-
-dylan = Sensor("DL43", "Dylan Larkin", 543543543543543)
-
-print("Player Name: " + dylan.get_name())
-print("Device ID: " + dylan.get_id())
-print("UUID: " + dylan.get_uuid())
