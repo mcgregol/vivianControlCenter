e90911425ff2724410164b2707cba9e012a8abcd
Steve McGregor <achieve@Steves-MacBook-Pro.local>
2023-08-03 22:07:04 -0400

    removed 'split()' arguments to avoid errors

--- scan.py
+++ scan.py
@@ -22,7 +22,7 @@ dictionary = {}
 
 # Parse each line into a key-value pair and store it in the dictionary, skipping the first two lines of output
 for line in lines[2:]:
-    key, value = line.split(' ')
+    key, value = line.split()
     dictionary[key] = value
 
 # Initialize a list for storing Sensor objects
