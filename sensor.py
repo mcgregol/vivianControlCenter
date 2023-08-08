7af84742bbc3644f92fabca781b51eb6e9ff329c
Liam McGregor <liam@pop-os.localdomain>
2023-08-07 23:39:36 -0400

    removed unnecessary method

--- sensor.py
+++ sensor.py
@@ -2,6 +2,3 @@ class Sensor:
     def __init__(self, id, uuid):
         self.id = id
         self.uuid = uuid
-
-    def list(self):
-        return "Sensor ID: " + self.id + "\nUUID: " + self.uuid
